6c3a8543b9cdd03db492281dff5eccfc614a5346
apolumestny <anpolumestny@gmail.com>
2023-09-13 16:17:25 +0300

    добавил тесты, переписал парсер без регулярок

--- parser/context_log_parser.py
+++ parser/context_log_parser.py
@@ -1,28 +1,36 @@
 """на вход передается context_log на выходе RawResponse или cachedRawResponse"""
-import re
 import json
 
 RAW_RESPONSE_PATTERNS: list[str] = ['cachedRawResponse', 'first raw response']
+
+
+class InvalidContextLog(Exception):
+    pass
 
 
 class LogStringWithRawRespNotExists(Exception):
     pass
 
 
-class RawResponseNotFound(Exception):
+class InvalidRawResponse(Exception):
     pass
 
 
 class ContextLogParser:
-    regular_exp = r'\{([^}]*)\}'
+    """принимает на вход строку с ответом от HLR и достает из нее  ответ провайдера"""
 
     def __init__(self, context_log: str) -> None:
         if not len(context_log):
             raise ValueError('empty context_log')
-        self.context_log = json.loads(context_log)
-        self.nested_context_log = self.get_nested_context_log()
-        self.raw_response_string = self.get_string_with_raw_response()
-        self.raw_response = self.get_raw_response()
+        try:
+            self.context_log = json.loads(context_log)
+        except json.JSONDecodeError:
+            raise InvalidContextLog
+
+        try:
+            self.raw_response = json.loads(self.get_raw_response())
+        except json.JSONDecodeError:
+            raise InvalidRawResponse
 
     def get_nested_context_log(self) -> list[str]:
         try:
@@ -30,16 +38,17 @@ class ContextLogParser:
         except KeyError:
             raise KeyError('context log did not contain nested context log')
 
-    def get_string_with_raw_response(self) -> str | None:
-        for log_string in self.nested_context_log:
+    def get_string_with_raw_response(self, nested_context_log: list[str]) -> str:
+        for log_string in nested_context_log:
             for pattern in RAW_RESPONSE_PATTERNS:
                 if pattern in log_string:
                     return log_string
         raise LogStringWithRawRespNotExists
 
-    def get_raw_response(self):
-        print(self.raw_response_string)
-        matches = re.findall(self.regular_exp, self.raw_response_string)
-        if matches:
-            return '{' + matches[0] + '}'
-        raise RawResponseNotFound
+    def get_raw_response(self) -> str:
+        nested_context_log = self.get_nested_context_log()
+        raw_string_response = self.get_string_with_raw_response(nested_context_log)
+        first_brace_index = raw_string_response.find('{')
+        last_brace_index = raw_string_response.rfind('}')
+        raw_resp = raw_string_response[first_brace_index:last_brace_index + 1]
+        return raw_resp
