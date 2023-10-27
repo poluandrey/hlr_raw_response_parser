c25478e64cb5b871a05f1d86eaae0006267bd3b1
apolumestny <anpolumestny@gmail.com>
2023-10-27 16:12:56 +0300

    add task handling via admin

--- hlr/admin.py
+++ hlr/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from hlr.models import Task, TaskDetail, HlrProduct
+from hlr.forms import TaskCreateForm
+from hlr.tasks import celery_task_handler
 
 
 @admin.register(Task)
@@ -11,7 +13,23 @@ class TaskAdmin(admin.ModelAdmin[Task]):
         'author',
         'insert_time',
         'last_update_time',
+
     ]
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Use special form during foo creation
+        """
+        defaults = {}
+        if obj is None:
+            defaults['form'] = TaskCreateForm
+        defaults.update(kwargs)
+        return super().get_form(request, obj, **defaults)
+
+    def save_model(self, request, obj: Task, form, change):
+        msisdn = form.cleaned_data['msisdn'].split(',')
+        super(TaskAdmin, self).save_model(request, obj, form, change)
+        celery_task_handler(task=obj, msisdns=msisdn, hlr_products_external_id=form.cleaned_data['hlr'])
 
 
 @admin.register(TaskDetail)
---
+++ hlr/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+from hlr.models import Task, HlrProduct
+
+
+class TaskCreateForm(forms.ModelForm):
+    msisdn = forms.CharField()
+    hlr = forms.MultipleChoiceField(required=False,
+                                    widget=forms.CheckboxSelectMultiple,
+                                    choices=HlrProduct.objects.all().values_list('product__external_product_id',
+                                                                                 'product__description'), )
+
+    class Meta:
+        model = Task
+
+        fields = [
+            'author',
+            'msisdn',
+            'hlr',
+        ]
+        widgets = {
+            'msisdn': forms.TextInput,
+            'hlr': forms.MultipleChoiceField(
+                required=False,
+                widget=forms.CheckboxSelectMultiple,
+                choices=HlrProduct.objects.all().values_list('id', 'product__description'),
+            ),
+        }
