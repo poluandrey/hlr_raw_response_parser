19adbd4e8121ad45e0848b94cde350cbcf64bad7
apolumestny <anpolumestny@gmail.com>
2025-04-23 15:06:25 +0300

    file uploading

--- hlr/forms.py
+++ hlr/forms.py
@@ -8,19 +8,33 @@ class TaskCreateForm(forms.ModelForm):
     msisdn = forms.CharField(required=False)
     file = forms.FileField(required=False)
     hlr = forms.ModelMultipleChoiceField(
-        required=True,
+        required=False,
         widget=forms.CheckboxSelectMultiple(attrs={
             'class': 'hlr-checkbox-select-multiply',
         }),
-        queryset=HlrProduct.objects,
+        queryset=HlrProduct.objects.filter(product__icontains='hlr'),
+        label="HLR продукты"
+    )
 
+    mnp = forms.ModelMultipleChoiceField(
+        required=False,
+        widget=forms.CheckboxSelectMultiple(attrs={
+            'class': 'mnp-checkbox-select-multiply',
+        }),
+        queryset=HlrProduct.objects.filter(product__icontains='mnp'),
+        label="MNP продукты"
     )
 
     def clean(self):
         cleaned_data = super().clean()
+        hlr = cleaned_data.get('hlr')
+        mnp = cleaned_data.get('mnp')
 
         if not (cleaned_data.get('msisdn') or cleaned_data.get('file')):
             raise ValidationError('fill msisdn or upload file')
+
+        if not mnp or hlr:
+            raise ValidationError('choose source')
 
     class Meta:
         model = Task
