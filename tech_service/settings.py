99366a3409e69d5b86ac399a078e74b7f73fdf0f
apolumestny <anpolumestny@gmail.com>
2025-05-19 16:06:20 +0300

    file uploading

--- tech_service/settings.py
+++ tech_service/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', False)
 
-FORCE_SCRIPT_NAME = '/mnp'
+# FORCE_SCRIPT_NAME = '/mnp'
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
